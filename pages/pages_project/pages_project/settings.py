728345373b893c92e8edf3c407a84e413c1e24b1
theshameem <theshameem.alam@gmail.com>
2021-05-19 09:59:38 +0600

    App settings updated

--- pages/pages_project/pages_project/settings.py
+++ pages/pages_project/pages_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages_project'
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
