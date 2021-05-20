827260d8acbeb82bee84b47b568fad1d4d03361c
theshameem <theshameem.alam@gmail.com>
2021-05-20 12:58:51 +0600

    Update Blog

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from blog.views import BlogListView, BlogDetailView, BlogCreateView
+from blog.views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+)
 
 urlpatterns = [
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
