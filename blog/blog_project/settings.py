6722b448b73c5c8109d41642ad0a1136b594f689
theshameem <theshameem.alam@gmail.com>
2021-05-20 09:11:38 +0600

    last update

--- blog/blog_project/settings.py
+++ blog/blog_project/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR/"templates"
+# STATICFILE_DIR = BASE_DIR/"static"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    BASE_DIR/'static',
+]
