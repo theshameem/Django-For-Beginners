65cb5e0db8fb6d1c648ba11986981e9cc5c311d3
theshameem <theshameem.alam@gmail.com>
2021-05-20 10:25:07 +0600

    Blog Detail views

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import BlogListView
+from blog.views import BlogListView, BlogDetailView
 
 urlpatterns = [
+    path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
 ]
