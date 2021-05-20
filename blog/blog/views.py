98783ff0e5e913865a9c25744c138604521df611
theshameem <theshameem.alam@gmail.com>
2021-05-20 10:12:37 +0600

    Experiencing

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView
 from blog.models import Post
-# Create your views here.
 
+# Create your views here.
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
