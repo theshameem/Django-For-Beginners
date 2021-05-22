32353efc1bf8d03a21acfd88513e7142a535bfb4
theshameem <theshameem.alam@gmail.com>
2021-05-22 12:02:18 +0600

    Custom User Model

--- news/users/admin.py
+++ news/users/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from users.forms import CustomUserCreationForm, CustomUserChangeForm
-from users.models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
--- news/users/forms.py
+++ news/users/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-from users.models import CustomUser
+from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email', 'age')
 
 class CustomUserChangeForm(UserChangeForm):
 
